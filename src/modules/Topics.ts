101bee68ad7f24e66c6afcf255d32629d54969d2
MGajewski <gajewski.mike@gmail.com>
2017-12-18 15:15:15 -0500

    Add seetopiccategories command.

--- src/modules/Topics.ts
+++ src/modules/Topics.ts
@@ -26,7 +26,15 @@ export class TopicsModule extends BaseModule
             this.addTopicCategory.bind(this)
         );
 
-        this.cmds.push(addTopicCategoryCmd);
+        const seeTopicCategoriesCmd: Command = new Command(
+            ['seetopiccategories', 'seetopiccats'],
+            [],
+            [PrismCommanderRole],
+            "See all supported topic categories.",
+            this.seeTopicCategories.bind(this)
+        );
+
+        this.cmds.push(addTopicCategoryCmd, seeTopicCategoriesCmd);
     }
 
     /**
@@ -66,6 +74,43 @@ export class TopicsModule extends BaseModule
             }
 
             message.channel.send("Topic category added.");
+        } 
+        catch (e) 
+        {
+            throw e;
+        }
+    }
+
+    /**
+     * See all supported topic categories.
+     * @param message The discord.js message instance.
+     * @param args Arguments of the command.
+     */
+    private async seeTopicCategories(message: Discord.Message, args?: any[])
+    {
+        try 
+        {
+            // Grab all topic categories from db.
+            const catRows: any[] = await this.db.all("SELECT CategoryId, PrimaryChannelId FROM TopicCategory", []);
+
+            // Grab all category/primary channel data from discord that corresponds to the db data.
+            const cats: Discord.CategoryChannel[] = message.guild.channels.filter(x => catRows.some(y => y.CategoryId === x.id)).array() as Discord.CategoryChannel[];
+            const primaryChannels: Discord.GuildChannel[] = message.guild.channels.filter(x => catRows.some(y => y.PrimaryChannelId === x.id)).array();
+
+            // Create rich embed for response.
+            const embed: Discord.RichEmbed = new Discord.RichEmbed();
+            embed.setColor([0, 0, 255]);
+            embed.setDescription("Supported Topic Categories");
+
+            catRows.forEach(x => {
+                const catDisplay: Discord.CategoryChannel = cats.find(y => y.id === x.CategoryId);
+                const primaryChannelDisplay: Discord.GuildChannel = primaryChannels.find(y => y.id === x.PrimaryChannelId);
+
+                embed.addField(catDisplay.name, `Primary channel: #${primaryChannelDisplay.name}.`);
+            });
+
+            // Send response.
+            message.channel.send(embed);
         } 
         catch (e) 
         {
